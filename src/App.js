5be84ee557452b33daf2f6e11058ab54658db847
Endy <7ofpentacles@gmail.com>
2021-09-28 14:12:55 +0700

    change actions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          meong guk guk
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload. bray bray bray
         </p>
         <a
